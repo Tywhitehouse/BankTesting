43cf6479fe94da1fe08906b440713d7eec18d1e8
Zachary Fry <zacharydf7@gmail.com>
2020-05-03 17:07:47 -0400

    Added some more tests for Account.withdraw and Customer.removeAccount

--- src/main/java/banking/BankingTests.java
+++ src/main/java/banking/BankingTests.java
@@ -16,8 +16,10 @@ public class BankingTests {
         bank = new Bank( "My Bank" );
         custID = bank.addCustomer("Bebop", "Rocksteady");
         Customer customer = bank.getCustomer( custID );
-        Account savings = customer.addSavingsAccount( 0.00, "My Savings Account" );
+        Account savings = customer.addSavingsAccount( 10.00, "My Savings Account" );
     }
+
+    //Account tests
 
     // Test a deposit of $10.00 works:
     @Test
@@ -69,6 +71,56 @@ public class BankingTests {
         assertEquals( finalBalance, initialBalance,
                 "Balance should be " +
                         (initialBalance) + "but was " + finalBalance );
+    }
+
+    // Test a withdraw of $10.00 works:
+    @Test
+    @DisplayName("Account.withdraw Tests")
+    void withdrawShouldDecreaseBalance () {
+        final double initialBalance = account.getBalance();
+        final double amount = 10.00;
+        account.withdraw(amount);
+        final double finalBalance = account.getBalance();
+        assertEquals( finalBalance, 0,
+                "Balance should be 0 but was " + finalBalance );
+    }
+
+    // Test a withdraw of $-5.00 is rejected and balance remains untouched:
+    @Test
+    @DisplayName("Account.withdraw Tests")
+    void negativeWithdrawShouldNotBeAccepted () {
+        final double initialBalance = account.getBalance();
+        final double amount = -5.00;
+        account.withdraw( amount );
+        final double finalBalance = account.getBalance();
+        assertEquals( finalBalance, initialBalance,
+                "Balance should be " +
+                        (initialBalance) + "but was " + finalBalance );
+    }
+
+    // Test a withdraw of $999,999,999 is rejected and balance remains untouched:
+    @Test
+    @DisplayName("Account.withdraw Tests")
+    void veryLargeWithdrawShouldNotBeAccepted () {
+        final double initialBalance = account.getBalance();
+        final double amount = 999999999;
+        account.withdraw(amount);
+        final double finalBalance = account.getBalance();
+        assertEquals( finalBalance, initialBalance,
+                "Balance should be " +
+                        (initialBalance) + "but was " + finalBalance );
+    }
+
+
+    //Customer tests
+
+    //Remove the account from user then try to get it, should be null.
+    @Test
+    @DisplayName("Customer.removeAccount Tests")
+    void removeAccountRemovesAccountFromCustomer () {
+        customer.removeAccount("1");
+        var customerAccount = customer.getAccount("1");
+        assertNull(customerAccount, "Did not receive the expected null account value.");
     }
 
 }
